9082df9d306d1a1a68b37b37967c480cc20f023e
fieryfalcon <rsudarsan369@gmail.com>
2023-03-25 15:07:47 +0530

    Made my first wagtail site

---
+++ 2023/Sudarsan/Wagtail_first_site/mysite/project01/project01/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-j_t_loq-1kktqk7civfc@n&fwq*6!=-bf&9()rkus*ob(&#wa7"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
